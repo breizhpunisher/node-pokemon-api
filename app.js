11eb49a471bb2a788ebfdd2399cb3775e5074727
breizhpunisher <potache53@hotmail.fr>
2023-04-28 12:53:56 +0200

    déploiement

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express')
-const morgan = require('morgan')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize')
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
        
 
 app
 .use(favicon(__dirname + '/favicon.ico'))
-.use(morgan('dev'))// middleware message console.
 .use(bodyParser.json())// parser en json.
 
 sequelize.initDb()
+
+app.get('/', (req, res) => {
+    res.json('Hello, HEROKU!!')
+})
 
 // Ici, nous mettrons nos futurs points de terminaison.
 require('./src/routes/findAllPokemons')(app)
